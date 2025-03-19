26eefc458ec1e3ad5effdcd4ecffbfd5852ffff6
karribalu <karri.balu123456@gmail.com>
2025-03-19 22:44:08 +0000

    feat: Add .env in backend

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,16 @@
 import uvicorn
 from fastapi import FastAPI
-
-
+from dotenv import load_dotenv
+from openai import OpenAI
+load_dotenv()
 app = FastAPI(
     title="Design Sage",
     description="Design Sage is a tool that helps you design your website",
     version="0.1.0",
 )
+
+openai_service = OpenAI(api_key=os.getenv(
+    "OPENAI_API_KEY"), base_url="https://api.x.ai/v1")
 
 
 @app.get("/")
